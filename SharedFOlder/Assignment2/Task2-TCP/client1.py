e08e37aae8f029fd86f64afc5658b414f3c4495c
Vinh Trung <Trung_thai@live.dk>
2021-10-27 14:55:20 +0200

    Fix

--- SharedFOlder/Assignment2/Task2-TCP/client1.py
+++ SharedFOlder/Assignment2/Task2-TCP/client1.py
@@ -5,13 +5,10 @@ import time
 HOST = '192.168.0.7'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-<<<<<<< HEAD
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-=======
+
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
->>>>>>> daa667765ddd95cd806ef50a30d959835b053a86
     incrementVal = 0
     print("Connecting")
     s.connect((HOST, PORT))
@@ -19,30 +16,6 @@ while True:
     print("Sending MSG")
     s.sendall(msg.encode())
     data = s.recv(1024)
-<<<<<<< HEAD
-    while True:
-        if not data:
-            print("No data Received")
-            break
-        else:
-            print("Received:", data)
-            recvVal = data.decode("UTF-8")
-            incVal = re.findall(r'\b\d+\b',recvVal)
-            incrementVal = int(incVal[0]) + 1
-            msg = "Increment this : " + str(incrementVal)
-            print("Sending next message")
-            time.sleep(2)
-            print(msg)
-            s.sendall(msg.encode())
-        if(incrementVal == 256):
-            break
-
-
-
-
-
-print('Received', repr(data))
-=======
     if not data:
         print("No data Received")
         break
@@ -60,4 +33,3 @@ print('Received', repr(data))
 
 
 
->>>>>>> daa667765ddd95cd806ef50a30d959835b053a86
