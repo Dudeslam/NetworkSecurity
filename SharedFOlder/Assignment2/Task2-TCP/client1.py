daa667765ddd95cd806ef50a30d959835b053a86
Dudeslam <trung_thai@live.dk>
2021-10-27 14:53:19 +0200

    still working on client

--- SharedFOlder/Assignment2/Task2-TCP/client1.py
+++ SharedFOlder/Assignment2/Task2-TCP/client1.py
@@ -2,35 +2,32 @@ import socket
 import re
 import time
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = '192.168.0.7'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        incrementVal = 0
-        print("Connecting")
-        s.connect((HOST, PORT))
-        msg = "Increment this : " + str(incrementVal)
-        print("Sending MSG")
-        s.sendall(msg.encode())
-        data = s.recv(1024)
-        if not data:
-            print("No data Received")
-            break
-        else:
-            print("Received:", data)
-            recvVal = data.decode("UTF-8")
-            incVal = re.findall(r'\b\d+\b',recvVal)
-            incrementVal = int(incVal[0]) + 1
-            msg = "Increment this : " + str(incrementVal)
-            print("Sending next message")
-            time.sleep(2)
-            s.sendall(msg.encode())
-
-
-
-
-    if(incrementVal == 256):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    incrementVal = 0
+    print("Connecting")
+    s.connect((HOST, PORT))
+    msg = "Increment this : " + str(incrementVal)
+    print("Sending MSG")
+    s.sendall(msg.encode())
+    data = s.recv(1024)
+    if not data:
+        print("No data Received")
         break
+    else:
+        print("Received:", data)
+        recvVal = data.decode("UTF-8")
+        incVal = re.findall(r'\b\d+\b',recvVal)
+        incrementVal = int(incVal[0]) + 1
+        msg = "Increment this : " + str(incrementVal)
+        print("Sending next message")
+        time.sleep(2)
+        s.sendall(msg.encode())
+        s.close()
+            
 
-print('Received', repr(data))
+
+
